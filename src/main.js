3f114918fa11d5ab64d1d880083e10b67d62fb81
Rui Santiago <ruisantiagomr@gmail.com>
2021-07-04 16:41:17 +0100

    refact play-pause toggle

--- src/main.js
+++ src/main.js
@@ -1,16 +1,2 @@
 const playBtn = document.getElementById("play-btn");
-playBtn.addEventListener("click", () => changeBtn(playBtn, "play"));
-
-const pauseBtn = document.getElementById("play-btn");
-pauseBtn.addEventListener("click", () => changeBtn(pauseBtn, "pause"));
-
-function changeBtn(btn, type) {
-  switch (type) {
-    case "pause":
-      btn.classList.remove("fa-pause");
-      btn.classList.add("fa-play");
-    case "play":
-      btn.classList.remove("fa-play");
-      btn.classList.add("fa-pause");
-  }
-}
+playBtn.addEventListener("click", () => playBtn.classList.toggle("fa-pause"));
